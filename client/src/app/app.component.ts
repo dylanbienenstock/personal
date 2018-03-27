79920b305865d751a910cad0d2563499a3d5de87
Dylan Bienenstock <dylanbienenstock@gmail.com>
2018-03-27 02:09:26 -0700

    Make page fade in on load

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent implements AfterViewInit {
+	title = 'app';
+
+	@ViewChild("content")
+	private contentRef: ElementRef;
+
+	ngAfterViewInit() {
+		this.contentRef.nativeElement.style.opacity = 1;
+	}
 }
