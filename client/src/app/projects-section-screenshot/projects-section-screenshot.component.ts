886acb97a099b1f346edb253a317bfa32560da39
Dylan Bienenstock <dylanbienenstock@gmail.com>
2018-03-28 14:36:08 -0700

    Setup basic projects section layout

--- client/src/app/projects-section-screenshot/projects-section-screenshot.component.ts
+++ client/src/app/projects-section-screenshot/projects-section-screenshot.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-projects-section-screenshot',
-  templateUrl: './projects-section-screenshot.component.html',
-  styleUrls: ['./projects-section-screenshot.component.scss']
+    selector: 'app-projects-section-screenshot',
+    templateUrl: './projects-section-screenshot.component.html',
+    styleUrls: ['./projects-section-screenshot.component.scss']
 })
 export class ProjectsSectionScreenshotComponent implements OnInit {
+    @Input() image: string;
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
--- client/src/app/projects-section/projects-section.component.ts
+++ client/src/app/projects-section/projects-section.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as projectsJson from "projects.json";
+
 @Component({
-  selector: 'app-projects-section',
-  templateUrl: './projects-section.component.html',
-  styleUrls: ['./projects-section.component.scss']
+    selector: 'app-projects-section',
+    templateUrl: './projects-section.component.html',
+    styleUrls: ['./projects-section.component.scss']
 })
 export class ProjectsSectionComponent implements OnInit {
+    projectSubsections = projectsJson;
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
-
+    ngOnInit() {
+        
+    }
 }
