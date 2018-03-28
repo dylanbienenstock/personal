ec035332671301366b883c25c875b159afc96609
Dylan Bienenstock <dylanbienenstock@gmail.com>
2018-03-27 21:42:54 -0700

    Fill skills section with placeholder info

--- client/src/app/skills-section/skills-section.component.ts
+++ client/src/app/skills-section/skills-section.component.ts
@@ -1,15 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-skills-section',
-  templateUrl: './skills-section.component.html',
-  styleUrls: ['./skills-section.component.scss']
+    selector: 'app-skills-section',
+    templateUrl: './skills-section.component.html',
+    styleUrls: ['./skills-section.component.scss']
 })
 export class SkillsSectionComponent implements OnInit {
+    constructor() { }
 
-  constructor() { }
+    skillSections = [
+         {
+             name: "Technologies", 
+             skills: [
+                "Node / Express",
+                "Socket.IO",
+                "Angular",
+                "MongoDB",
+                "ASP NET Core",
+                "Entity Framework",
+                "Django",
+                "Flask"
+            ],
+        },
+        {
+            name: "Languages", 
+            skills: [
+                "HTML",
+                "Javascript / Typescript",
+                "CSS / SCSS",
+                "C#",
+                "Python",
+                "Lua"
+            ],
+        },
+        {
+            name: "Miscellaneous", 
+            skills: [
+                "Game Engine Design",
+                "3D Level Design",
+                "3D Modeling",
+                "Graphic Design"
+            ]
+        }
+    ]
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+
+    }
 
 }
