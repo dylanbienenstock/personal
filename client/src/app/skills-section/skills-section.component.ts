8b5a94260596bf1d615e157668e422c7f42e3aa7
Dylan Bienenstock <dylanbienenstock@gmail.com>
2018-03-28 13:58:57 -0700

    Move skills data into skills.json

--- client/src/app/skills-section/skills-section.component.ts
+++ client/src/app/skills-section/skills-section.component.ts
@@ -1,116 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as skillsJson from "skills.json";
+
 @Component({
     selector: 'app-skills-section',
     templateUrl: './skills-section.component.html',
     styleUrls: ['./skills-section.component.scss']
 })
 export class SkillsSectionComponent implements OnInit {
-    skillSubsections: any = [
-        {
-            name: "Technologies",
-            skills: [
-                {
-                    name: "Node + Express",
-                    level: 1
-                },
-                {
-                    name: "Socket.IO",
-                    level: 2
-                },
-                {
-                    name: "Angular",
-                    level: 1
-                },
-                {
-                    name: "MongoDB",
-                    level: 0
-                },
-                {
-                    name: "ASP NET Core",
-                    level: 2
-                },
-                {
-                    name: "Entity Framework",
-                    level: 1
-                },
-                {
-                    name: "Django",
-                    level: 2
-                },
-                {
-                    name: "jQuery",
-                    level: 2
-                }
-            ],
-        },
-        {
-            name: "Languages",
-            skills: [
-                {
-                    name: "C / C++",
-                    level: 0
-                },
-                {
-                    name: "Javascript / Typescript",
-                    level: 2
-                },
-                {
-                    name: "CSS / SCSS",
-                    level: 2
-                },
-                {
-                    name: "C#",
-                    level: 2
-                },
-                {
-                    name: "Python",
-                    level: 1
-                },
-                {
-                    name: "Lua",
-                    level: 2
-                },
-                {
-                    name: "Java",
-                    level: 1
-                },
-                {
-                    name: "Objective-C",
-                    level: 0
-                }
-            ]
-        },
-        {
-            name: "Miscellaneous",
-            skills: [
-                {
-                    name: "Game Engine Design",
-                    level: 1
-                },
-                {
-                    name: "3D Level Design",
-                    level: 2
-                },
-                {
-                    name: "3D Modeling",
-                    level: 1
-                },
-                {
-                    name: "Graphic Design",
-                    level: 0
-                },
-                {
-                    name: "Electrical Engineering",
-                    level: 0
-                },
-                {
-                    name: "UX / UI Design",
-                    level: 1
-                }
-            ]
-        }
-    ]
+    skillSubsections = skillsJson;
 
     constructor()
     {
@@ -131,5 +29,4 @@ export class SkillsSectionComponent implements OnInit {
     ngOnInit() {
         
     }
-
 }
