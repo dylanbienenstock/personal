b7a31745cb9e8cdd990af46a3dd53f1c4e6f6a60
Dylan Bienenstock <dylanbienenstock@gmail.com>
2018-03-28 16:40:47 -0700

    Finish screenshot component

--- client/src/app/projects-section-screenshot/projects-section-screenshot.component.ts
+++ client/src/app/projects-section-screenshot/projects-section-screenshot.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ProjectsSectionScreenshotComponent implements OnInit {
     @Input() image: string;
+    showTitleBar: boolean = false;
 
     constructor() { }
 
