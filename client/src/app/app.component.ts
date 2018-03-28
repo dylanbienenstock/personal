c1ac0904d798a16b0167e976fd262fcdcee978cd
Dylan Bienenstock <dylanbienenstock@gmail.com>
2018-03-28 02:01:30 -0700

    Add scroll handler

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit, HostListener } from '@angular/core';
 
 @Component({
 	selector: 'app-root',
@@ -9,10 +9,22 @@ import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 export class AppComponent implements AfterViewInit {
 	title = 'app';
 
-	@ViewChild("content")
-	private contentRef: ElementRef;
+    @ViewChild("content")
+    private contentRef: ElementRef;
+
+    @ViewChild("header")
+    private headerRef: ElementRef;
 
 	ngAfterViewInit() {
 		this.contentRef.nativeElement.style.opacity = 1;
-	}
+    }
+    
+    @HostListener("window:scroll", ["$event"])
+    onScrollEvent($event) {
+        let header = this.contentRef.nativeElement.children[0];
+
+        let scrollY = window.pageYOffset ||
+            document.documentElement.scrollTop ||
+            document.body.scrollTop || 0;
+    } 
 }
