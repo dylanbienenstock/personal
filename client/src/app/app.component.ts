fca6ef922f9dca08a7eaea73058c25c0d68829bd
Dylan Bienenstock <dylanbienenstock@gmail.com>
2018-03-28 22:01:31 -0700

    Remove scroll listener

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -12,19 +12,7 @@ export class AppComponent implements AfterViewInit {
     @ViewChild("content")
     private contentRef: ElementRef;
 
-    @ViewChild("header")
-    private headerRef: ElementRef;
-
 	ngAfterViewInit() {
 		this.contentRef.nativeElement.style.opacity = 1;
     }
-    
-    @HostListener("window:scroll", ["$event"])
-    onScrollEvent($event) {
-        let header = this.contentRef.nativeElement.children[0];
-
-        let scrollY = window.pageYOffset ||
-            document.documentElement.scrollTop ||
-            document.body.scrollTop || 0;
-    } 
 }
