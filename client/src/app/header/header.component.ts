51d9635219f6a129e5ae8792b572cc4b87c8c9a4
Dylan Bienenstock <dylanbienenstock@gmail.com>
2018-03-29 01:42:25 -0700

    Add scroll events

--- client/src/app/header/header.component.ts
+++ client/src/app/header/header.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { ScrollService } from '../scroll.service';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+	selector: 'app-header',
+	templateUrl: './header.component.html',
+	styleUrls: ['./header.component.scss']
 })
+
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+	constructor(private _scroll: ScrollService) { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
 
+	}
 }
--- client/src/app/home-page/home-page.component.ts
+++ client/src/app/home-page/home-page.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { ScrollService } from '../scroll.service';
 
 @Component({
-  selector: 'app-home-page',
-  templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+	selector: 'app-home-page',
+	templateUrl: './home-page.component.html',
+	styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+	constructor(private _scroll: ScrollService) { }
 
-  ngOnInit() {
-  }
-
+	ngOnInit() {
+		
+	}
 }
