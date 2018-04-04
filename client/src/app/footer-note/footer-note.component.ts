906103724b1f3a9f6ead7b6f335290f74ba51ade
Dylan Bienenstock <dylanbienenstock@gmail.com>
2018-04-04 16:02:37 -0700

    Add button events

--- client/src/app/footer-note/footer-note.component.ts
+++ client/src/app/footer-note/footer-note.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-footer-note',
-  templateUrl: './footer-note.component.html',
-  styleUrls: ['./footer-note.component.scss']
+    selector: 'app-footer-note',
+    templateUrl: './footer-note.component.html',
+    styleUrls: ['./footer-note.component.scss']
 })
 export class FooterNoteComponent implements OnInit {
+    constructor() { }
 
-  constructor() { }
+    ngOnInit() {
+    }
 
-  ngOnInit() {
-  }
+    onSubmit($event) {
+        $event.preventDefault();
+    }
 
+    onClear($event) {
+        $event.preventDefault();
+    }
 }
