bf892ed049a73b56c0b85e6f0930def80116d651
Dylan Bienenstock <dylanbienenstock@gmail.com>
2018-04-04 21:34:27 -0700

    Add fake downloads

--- client/src/app/footer-downloads/footer-downloads.component.ts
+++ client/src/app/footer-downloads/footer-downloads.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-footer-downloads',
-  templateUrl: './footer-downloads.component.html',
-  styleUrls: ['./footer-downloads.component.scss']
+	selector: 'app-footer-downloads',
+	templateUrl: './footer-downloads.component.html',
+	styleUrls: ['./footer-downloads.component.scss']
 })
 export class FooterDownloadsComponent implements OnInit {
+	constructor() { }
 
-  constructor() { }
+	downloads: Object[] = [
+		{ name: "fake_download.txt", size: Math.round(Math.random() * 1023) },
+		{ name: "fake_download.txt", size: Math.round(Math.random() * 1023) },
+		{ name: "fake_download.txt", size: Math.round(Math.random() * 1023) },
+		{ name: "fake_download.txt", size: Math.round(Math.random() * 1023) },
+		{ name: "fake_download.txt", size: Math.round(Math.random() * 1023) },
+		{ name: "fake_download.txt", size: Math.round(Math.random() * 1023) },
+		{ name: "fake_download.txt", size: Math.round(Math.random() * 1023) },
+	];
 
-  ngOnInit() {
-  }
-
+	ngOnInit() {
+	}
 }
