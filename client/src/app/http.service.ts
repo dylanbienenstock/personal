ea606a8ae12ba4b37669e306a31cae4d66f755fd
Dylan Bienenstock <dylanbienenstock@gmail.com>
2018-04-06 19:59:27 -0700

    Make downloads folder sync to client

--- client/src/app/http.service.ts
+++ client/src/app/http.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class HttpService {
+    constructor(private _httpClient: HttpClient) { }
 
-  constructor() { }
-
+    getDownloads() {
+        return this._httpClient.get("downloads");
+    }
 }
